e17c3bcab59ee1b9a56eca068b2f0998ebd17cf8
bene-s-dev <development@stehmans.de>
2026-05-06 12:45:23 +0200

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Meine Super App',
+        short_name: 'SuperApp',
+        theme_color: '#3b82f6',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
