63ad78e188a732856bdd084d20483efe37d1d5df
bene-s-dev <development@stehmans.de>
2026-05-10 01:01:17 +0200

    App.tsx erstellen

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ background: '#0f172a', color: 'white', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyCenter: 'center' }}>
+      <h1>System läuft!</h1>
+    </div>
+  );
+}
