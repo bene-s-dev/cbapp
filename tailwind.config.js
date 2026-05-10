2e95ec02e7025c8df82628bf969d8fc7669317b4
bene-s-dev <development@stehmans.de>
2026-05-10 02:04:44 +0200

    Aktualisieren von tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/App.tsx",
+    "./src/main.tsx"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'duo-card': '#271a3c',
+      }
+    },
   },
   plugins: [],
 }
